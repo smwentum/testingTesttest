d3df9332f4ce8b14362ef5f11d1b6c7a86efc764
sai wentum <michael.wentum@gmail.com>
2019-08-17 18:26:37 -0400

    fixing tests

--- sketch.test.js
+++ sketch.test.js
@@ -4,6 +4,6 @@ test("adds 1 + 2 to equal 3", () => {
   expect(funcs.sum(1, 2)).toBe(3);
 });
 
-test("adds 2 -1 to equal 1", () => {
+test("difference 2 -1 to equal 1", () => {
   expect(funcs.difference(2, 1)).toBe(1);
 });
