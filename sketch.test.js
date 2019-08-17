ff1bddbf280e452dc63543d379247cb164ec4c6b
sai wentum <michael.wentum@gmail.com>
2019-08-17 18:43:37 -0400

    broken

--- sketch.test.js
+++ sketch.test.js
@@ -4,6 +4,14 @@ test("adds 1 + 2 to equal 3", () => {
   expect(funcs.sum(1, 2)).toBe(3);
 });
 
+test("adds 2 + 2 to equal 4", () => {
+  expect(funcs.sum(2, 2)).toBe(3);
+});
+
 test("difference 2 -1 to equal 1", () => {
   expect(funcs.difference(2, 1)).toBe(1);
+});
+
+test("difference 1 -2 to equal -1", () => {
+  expect(funcs.difference(1, 2)).toBe(-1);
 });
